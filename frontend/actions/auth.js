48f8da2f09f5311b401a7ba7511252f8b206e560
maclinz <tafadzwamucheke@gmail.com>
2022-02-06 01:07:48 +0000

    signup

--- frontend/actions/auth.js
+++ frontend/actions/auth.js
@@ -1,2 +1,7 @@
 import fetch from 'isomorphic-fetch';
 
+import {API} from '../config';
+
+export const signUp = (user) => {
+    return fetch(`${API}/signup`, {})
+}
--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,5 @@
 import getConfig from "next/config";
 const { publicRuntimeConfig } = getConfig();
 
-export const API = publicRuntimeConfig.API_PRODUCTION ? publicRuntimeConfig.API_PRODUCTION : 'http://localhost:8000';
+export const API = publicRuntimeConfig.API_PRODUCTION ? publicRuntimeConfig.API_PRODUCTION : publicRuntimeConfig.API_DEVELOPMENT;
 export const APP_NAME = publicRuntimeConfig.APP_NAME;
